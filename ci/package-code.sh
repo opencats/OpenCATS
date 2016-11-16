204756f5a7a3e88745808ff1075fd2d2edf8c3c9
Matias Lespiau <lespiau@gmail.com>
2016-11-16 18:48:46 -0300

    Rename release file

--- ci/package-code.sh
+++ ci/package-code.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -ev
 if [ "${TRAVIS_TAG}" != "false" ]; then
-	tar -cvzf /tmp/$TRAVIS_TAG.tar.gz -C $TRAVIS_BUILD_DIR .
-	cp /tmp/$TRAVIS_TAG.tar.gz $TRAVIS_BUILD_DIR/$TRAVIS_TAG.tar.gz
+	tar -cvzf /tmp/$TRAVIS_TAG-full.tar.gz -C $TRAVIS_BUILD_DIR .
+	cp /tmp/$TRAVIS_TAG-full.tar.gz $TRAVIS_BUILD_DIR/$TRAVIS_TAG-full.tar.gz
 fi
