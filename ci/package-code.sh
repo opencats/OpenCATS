a221a4d5610f060a175e603fb1bcf4af5f354f32
Matias Lespiau <lespiau@gmail.com>
2016-11-17 00:12:09 -0300

    Create and release zip file Rename .tar.gz file to include opencats as part of the name.

--- ci/package-code.sh
+++ ci/package-code.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -ev
+
 if [ "${TRAVIS_TAG}" != "false" ]; then
-	tar -cvzf /tmp/$TRAVIS_TAG-full.tar.gz -C $TRAVIS_BUILD_DIR .
-	cp /tmp/$TRAVIS_TAG-full.tar.gz $TRAVIS_BUILD_DIR/$TRAVIS_TAG-full.tar.gz
+	tar -cvzf /tmp/opencats-$TRAVIS_TAG-full.tar.gz -C $TRAVIS_BUILD_DIR .
+	zip -r /tmp/opencats-$TRAVIS_TAG-full.zip $TRAVIS_BUILD_DIR
+	cp /tmp/opencats-$TRAVIS_TAG-full.tar.gz $TRAVIS_BUILD_DIR
+	cp /tmp/opencats-$TRAVIS_TAG-full.zip $TRAVIS_BUILD_DIR
 fi
