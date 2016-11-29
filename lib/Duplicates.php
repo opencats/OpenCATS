6b37f0a1811c44591a2f5e33c5eceb5fb0a2e605
skrchnavy <svetozar.krchnavy@anritsu.com>
2016-11-29 19:52:16 +0100

    corrected header of file to follow license

--- lib/Duplicates.php
+++ lib/Duplicates.php
@@ -1,34 +1,14 @@
 <?php
 /**
- * CATS
- * Candidates Library
- *
- * Copyright (C) 2005 - 2007 Cognizo Technologies, Inc.
- *
- *
- * The contents of this file are subject to the CATS Public License
- * Version 1.1a (the "License"); you may not use this file except in
- * compliance with the License. You may obtain a copy of the License at
- * http://www.catsone.com/.
- *
- * Software distributed under the License is distributed on an "AS IS"
- * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
- * License for the specific language governing rights and limitations
- * under the License.
- *
- * The Original Code is "CATS Standard Edition".
- *
- * The Initial Developer of the Original Code is Cognizo Technologies, Inc.
- * Portions created by the Initial Developer are Copyright (C) 2005 - 2007
- * (or from the year in which this file was created to the year 2007) by
- * Cognizo Technologies, Inc. All Rights Reserved.
- *
- *
- * @package    CATS
- * @subpackage Library
- * @copyright Copyright (C) 2005 - 2007 Cognizo Technologies, Inc.
- * @version    $Id: Candidates.php 3813 2007-12-05 23:16:22Z brian $
- */
+* Duplicates Module
+* @package OpenCATS
+* @subpackage Library
+* @copyright (C) OpenCats
+* @license GNU/GPL, see license.txt
+* OpenCATS is free software; you can redistribute it and/or
+* modify it under the terms of the GNU General Public License 2
+* as published by the Free Software Foundation.
+*/
 
 include_once('./lib/Attachments.php');
 include_once('./lib/Pipelines.php');
