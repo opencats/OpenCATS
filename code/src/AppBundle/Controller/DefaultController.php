691e51cbd432e5c0b7e5675f793bbd684c804572
Matias Lespiau <lespiau@gmail.com>
2016-12-07 10:03:50 -0300

    Remove / route from DefaultController

--- code/src/AppBundle/Controller/DefaultController.php
+++ code/src/AppBundle/Controller/DefaultController.php
@@ -9,7 +9,7 @@ use Symfony\Component\HttpFoundation\Request;
 class DefaultController extends Controller
 {
     /**
-     * @Route("/", name="homepage")
+     * @Route("/test", name="homepage")
      */
     public function indexAction(Request $request)
     {
