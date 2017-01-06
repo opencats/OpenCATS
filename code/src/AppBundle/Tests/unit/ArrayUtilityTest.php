34d4a95181389b6171db585541096a68f83b187e
Matias Lespiau <lespiau@gmail.com>
2017-01-06 18:07:21 -0300

    Apply Symphony + Codeception standards to ArrayUtility unit test

--- code/src/AppBundle/Tests/unit/ArrayUtilityTest.php
+++ code/src/AppBundle/Tests/unit/ArrayUtilityTest.php
@@ -1,9 +1,9 @@
 <?php
-use PHPUnit\Framework\TestCase;
+namespace AppBundle;
 
 include_once(LEGACY_ROOT . '/lib/ArrayUtility.php');
 
-class ArrayUtilityTest extends TestCase
+class ArrayUtilityTest extends \PHPUnit_Framework_TestCase
 {
     /* Tests for implodeRange(). */
     function testImplodeRange()
@@ -17,31 +17,31 @@ class ArrayUtilityTest extends TestCase
             'Five'
         );
 
-        $result = ArrayUtility::implodeRange(' ', $pieces, 0, 5);
+        $result = \ArrayUtility::implodeRange(' ', $pieces, 0, 5);
         $this->assertSame($result, 'Zero One Two Three Four Five');
 
-        $result = ArrayUtility::implodeRange(' ', $pieces, 0, 4);
+        $result = \ArrayUtility::implodeRange(' ', $pieces, 0, 4);
         $this->assertSame($result, 'Zero One Two Three Four');
 
-        $result = ArrayUtility::implodeRange(' ', $pieces, 1, 4);
+        $result = \ArrayUtility::implodeRange(' ', $pieces, 1, 4);
         $this->assertSame($result, 'One Two Three Four');
 
-        $result = ArrayUtility::implodeRange(' ', $pieces, 1, 3);
+        $result = \ArrayUtility::implodeRange(' ', $pieces, 1, 3);
         $this->assertSame($result, 'One Two Three');
 
-        $result = ArrayUtility::implodeRange(' ', $pieces, 2, 3);
+        $result = \ArrayUtility::implodeRange(' ', $pieces, 2, 3);
         $this->assertSame($result, 'Two Three');
 
-        $result = ArrayUtility::implodeRange(' ', $pieces, 2, 2);
+        $result = \ArrayUtility::implodeRange(' ', $pieces, 2, 2);
         $this->assertSame($result, 'Two');
 
-        $result = ArrayUtility::implodeRange(' ', $pieces, 0, 6);
+        $result = \ArrayUtility::implodeRange(' ', $pieces, 0, 6);
         $this->assertSame($result, 'Zero One Two Three Four Five');
 
-        $result = ArrayUtility::implodeRange(' ', $pieces, -500, 500);
+        $result = \ArrayUtility::implodeRange(' ', $pieces, -500, 500);
         $this->assertSame($result, 'Zero One Two Three Four Five');
 
-        $result = ArrayUtility::implodeRange(', ', $pieces, -500, 500);
+        $result = \ArrayUtility::implodeRange(', ', $pieces, -500, 500);
         $this->assertSame($result, 'Zero, One, Two, Three, Four, Five');
     }
 }
