b5e413c952100b3a370cd87895b0a73aaa8df8ce
Matias Lespiau <lespiau@gmail.com>
2017-01-09 18:14:12 -0300

    Increase wait timeout

--- test/runAllTests.sh
+++ test/runAllTests.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 cd /var/www/
-dockerize -wait tcp://opencats_test_mariadb:3306 -wait http://opencats_test_web:80 -timeout 30s
+dockerize -wait tcp://opencats_test_mariadb:3306 -wait http://opencats_test_web:80 -timeout 60s
 php modules/tests/waitForDb.php
 cat config.php
 cd /var/www/code
