24ba9285203a4fdc94da611ee4d43223eb9cb0ca
Matias Lespiau <lespiau@gmail.com>
2017-01-10 18:07:00 -0300

    Add debug information

--- test/runAllTests.sh
+++ test/runAllTests.sh
@@ -4,6 +4,7 @@ dockerize -wait tcp://opencats_test_mariadb:3306 -wait http://opencats_test_web:
 php modules/tests/waitForDb.php
 cat config.php
 cd /var/www/code
+ls -la
 ./vendor/bin/codecept run acceptance activities.feature -vvv
 # ./vendor/bin/behat -v -c ./test/behat.yml --suite="default"
 # ./vendor/bin/behat -v -c ./test/behat.yml --suite="security"
