51877dd5706af5c749d7e05a0ed9ae8bfdb0f477
Matias Lespiau <lespiau@gmail.com>
2017-01-11 17:32:15 -0300

    Fix debugging info

--- test/runAllTests.sh
+++ test/runAllTests.sh
@@ -4,8 +4,6 @@ dockerize -wait tcp://opencats_test_mariadb:3306 -wait http://opencats_test_web:
 php modules/tests/waitForDb.php
 cat config.php
 cd /var/www/code
-docker --version
-docker info
 whoami
 ls -la /var/www/
 ls -la
