a7709b0da0585b050e021752d6501fa62fa17f54
Matias Lespiau <lespiau@gmail.com>
2017-01-28 16:35:22 -0300

    Fix db issues when running unit tests

--- code/src/AppBundle/tests/unit/_bootstrap.php
+++ code/src/AppBundle/tests/unit/_bootstrap.php
@@ -4,7 +4,7 @@ define('LEGACY_ROOT', dirname(dirname(dirname(dirname(dirname(dirname(__FILE__))
 define('DATABASE_USER', 'dev');
 define('DATABASE_PASS', 'dev');
 define('DATABASE_NAME', 'cats_unittestdb');
-define('DATABASE_HOST', 'unittestdb:3307');
+define('DATABASE_HOST', 'unittestdb');
 define('OFFSET_GMT', 2);
 define('SQL_CHARACTER_SET', 'utf8');
 define('CATS_SLAVE', false);
@@ -12,3 +12,20 @@ define('CATS_SLAVE', false);
 include_once (LEGACY_ROOT . '/vendor/autoload.php');
 include_once (LEGACY_ROOT . '/code/vendor/autoload.php');
 include_once (LEGACY_ROOT . '/constants.php');
+$canConnectAndSelectDb = false;
+$count = 30;
+while (!$canConnectAndSelectDb && $count > 0)
+{
+    $connection = @mysql_connect(
+        DATABASE_HOST, DATABASE_USER, DATABASE_PASS
+    );
+    if ($connection)
+    {
+        $canConnectAndSelectDb = true;
+    }
+    sleep(1);
+    --$count;
+}
+if (!$canConnectAndSelectDb) {
+    echo "Timeout while waiting for the DB and database.\n";
+}
