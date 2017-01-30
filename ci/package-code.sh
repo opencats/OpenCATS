6eeac3e625ec6a753d61256cfbfc1d3519f51411
Matias Lespiau <lespiau@gmail.com>
2017-01-30 17:10:27 -0300

    Do not include cache, session and logs folders in package distribution

--- ci/package-code.sh
+++ ci/package-code.sh
@@ -2,7 +2,7 @@
 set -ev
 
 if [ "${TRAVIS_TAG}" != "false" ]; then
-	tar -czf /tmp/opencats-$TRAVIS_TAG-full.tar.gz --exclude=INSTALL_BLOCK -C $TRAVIS_BUILD_DIR .
+	tar -czf /tmp/opencats-$TRAVIS_TAG-full.tar.gz --exclude=INSTALL_BLOCK --exclude=./code/var/cache --exclude=./code/var/logs --exclude=./code/var/sessions -C $TRAVIS_BUILD_DIR .
 	zip -q -x INSTALL_BLOCK -r /tmp/opencats-$TRAVIS_TAG-full.zip $TRAVIS_BUILD_DIR
 	cp /tmp/opencats-$TRAVIS_TAG-full.tar.gz $TRAVIS_BUILD_DIR
 	cp /tmp/opencats-$TRAVIS_TAG-full.zip $TRAVIS_BUILD_DIR
