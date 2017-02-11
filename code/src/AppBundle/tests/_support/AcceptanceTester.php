eeba751014e7f0fd2bfa915dd04d107667ff11f8
Matias Lespiau <lespiau@gmail.com>
2017-02-11 19:38:19 -0300

    AcceptanceTest: Remove try catch using for debbuing

--- code/src/AppBundle/tests/_support/AcceptanceTester.php
+++ code/src/AppBundle/tests/_support/AcceptanceTester.php
@@ -455,28 +455,23 @@ class AcceptanceTester extends Actor
      */
     public function iShouldNotHavePermission()
     {
-        try {
-            if($this->accessLevel == "DISABLED")
+        if($this->accessLevel == "DISABLED")
+        {
+            $this->theResponseShouldContain("opencats - Login");
+            return;
+        }
+        $expectedTexts = array("You don't have permission", "Invalid user level for action", "You are not allowed to change your password.");
+        $response = $this->getVisibleText();
+
+        foreach ($expectedTexts as &$text)
+        {
+            $position = strpos($response, $text);
+            if($position !== false)
             {
-                $this->theResponseShouldContain("opencats - Login");
                 return;
             }
-            $expectedTexts = array("You don't have permission", "Invalid user level for action", "You are not allowed to change your password.");
-            $response = $this->getVisibleText();
-
-            foreach ($expectedTexts as &$text)
-            {
-                $position = strpos($response, $text);
-                if($position !== false)
-                {
-                    return;
-                }
-            }
-            throw new Fail("'".$expectedTexts[0]."' was not found in the response from this request and it should be", $this->getSession());
-
-        } catch (\Exception $e) {
-            print_r($e->getMessage());
         }
+        throw new \Exception("'".$expectedTexts[0]."' was not found in the response from this request and it should be", $this->getSession());
     }
 
     /**
@@ -484,13 +479,8 @@ class AcceptanceTester extends Actor
      */
     public function iDoPOSTRequestOnUrl($url)
     {
-        try {
-            $this->amOnPage($url);
-            $this->submitForm('form', array('postback' => 'postback'));
-        } catch (\Exception $e) {
-            print_r($e->getMessage());
-        }
-
+        $this->amOnPage($url);
+        $this->submitForm('form', array('postback' => 'postback'));
     }
 
     /**
