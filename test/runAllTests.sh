99e92276fc8738ef57931fa6f9bda0f723986542
Matias Lespiau <lespiau@gmail.com>
2017-04-21 21:41:19 -0500

    Remove test/config.php as it’s now handled with environment variables

--- test/runAllTests.sh
+++ test/runAllTests.sh
@@ -2,6 +2,5 @@
 cd /var/www/
 dockerize -wait tcp://opencats_test_mariadb:3306 -wait http://opencats_test_web:80 -timeout 60s
 php modules/tests/waitForDb.php
-cat config.php
 cd /var/www/code
 ./vendor/bin/codecept run -c src/AppBundle/ acceptance
