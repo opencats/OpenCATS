7b808d5d1c8f958ec2ce52b3002d3a1cf443fd19
Matias Lespiau <lespiau@gmail.com>
2017-04-29 01:40:07 -0300

    Remove hardcoded configuration for tests

--- code/src/AppBundle/tests/acceptance/_bootstrap.php
+++ code/src/AppBundle/tests/acceptance/_bootstrap.php
@@ -1,11 +1,6 @@
 <?php
 // Here you can initialize variables that will be available to your tests
-// Here you can initialize variables that will be available to your tests
 define('LEGACY_ROOT', dirname(dirname(dirname(dirname(dirname(dirname(__FILE__)))))));
-define('DATABASE_USER', 'dev');
-define('DATABASE_PASS', 'dev');
-define('DATABASE_NAME', 'cats_test');
-define('DATABASE_HOST', 'opencatsdb:3306');
 define('OFFSET_GMT', 2);
 define('SQL_CHARACTER_SET', 'utf8');
 define('CATS_SLAVE', false);
