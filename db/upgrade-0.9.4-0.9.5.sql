5180ef88fa9112555256f2de3449336a2531a26d
skrchnavy <svetozar.krchnavy@anritsu.com>
2017-06-27 16:55:45 +0200

    added indexes to duplicates table

--- db/upgrade-0.9.4-0.9.5.sql
+++ db/upgrade-0.9.4-0.9.5.sql
@@ -1,6 +1,8 @@
 CREATE TABLE `candidate_duplicates` (
   `old_candidate_id` int(11) NOT NULL,
   `new_candidate_id` int(11) NOT NULL,
-  `site_id` int(11) NOT NULL,    
-  PRIMARY KEY (`old_candidate_id`, `new_candidate_id`)
-  ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+  `site_id` int(11) NOT NULL,
+  PRIMARY KEY (`old_candidate_id`, `new_candidate_id`),
+  KEY `IDX_old_candidate_id` (`old_candidate_id`),
+  KEY `IDX_new_candidate_id` (`new_candidate_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
--- test/data/test.sql
+++ test/data/test.sql
@@ -295,9 +295,11 @@ DROP TABLE IF EXISTS `candidate_duplicates`;
 CREATE TABLE `candidate_duplicates` (
   `old_candidate_id` int(11) NOT NULL,
   `new_candidate_id` int(11) NOT NULL,
-  `site_id` int(11) NOT NULL,    
-  PRIMARY KEY (`old_candidate_id`, `new_candidate_id`)
-  ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+  `site_id` int(11) NOT NULL,
+  PRIMARY KEY (`old_candidate_id`, `new_candidate_id`),
+  KEY `IDX_old_candidate_id` (`old_candidate_id`),
+  KEY `IDX_new_candidate_id` (`new_candidate_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
 -- Table structure for table `candidate_joborder`
