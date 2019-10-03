de76379a566e2316236a06706cdec677805376f3
amaisonneuve <amaisonneuve@911secure.com>
2019-10-03 09:10:51 -0400

    Revert "Fixed #451 there was an issue with the installer, this was an issue with the the mysqli call that now now changed how errors number and error descriptions are captured"
    This reverts commit a924d087caeb2e91ebab1f7a6e73f64d75267729.

--- modules/install/ajax/ui.php
+++ modules/install/ajax/ui.php
@@ -1115,13 +1115,13 @@ function MySQLQuery($query, $ignoreErrors = false)
     $queryResult = mysqli_query($mySQLConnection, $query);
     if (!$queryResult && !$ignoreErrors)
     {
-        if (!$mySQLConnection->connect_errno)
+				$error = "errno: " . $queryResult->connect_errno . ", ";
+				$error .= "error: " . $queryResult->connect_error;
+
+        if ($error == 'Query was empty')
         {
             return $queryResult;
         }
-
-        $error = "errno: " . $mySQLConnection->connect_errno . ", ";
-        $error .= "error: " . $mySQLConnection->connect_error;
 
         die (
             '<p style="background: #ec3737; padding: 4px; margin-top: 0; font:'
