6b30ea59e7eea20f8034947c472d31d786ee341d
russellh@ysmail.net <russellh@ysmail.net>
2024-10-01 18:42:21 +0100

    add error handling:securityTestData & runAllTests

--- test/runAllTests.sh
+++ test/runAllTests.sh
@@ -1,8 +1,21 @@
 #!/bin/sh -x
 cd /var/www/public/
 dockerize -wait tcp://opencats_test_mariadb:3306 -wait http://opencats_test_web:80 -timeout 30s
+
+# Check if MySQL is accessible
+if ! mysql --host=opencatsdb --user=dev --password=dev -e "SHOW DATABASES"; then
+    echo "ERROR: Unable to connect to opencatsdb."
+    exit 1
+fi
+
 php modules/tests/waitForDb.php
+
+# Check config.php for potential issues
 cat config.php
+
 ./vendor/bin/phpunit src/OpenCATS/Tests/IntegrationTests
 ./vendor/bin/behat -v -c ./test/behat.yml --suite="default"
 ./vendor/bin/behat -v -c ./test/behat.yml --suite="security"
+
+
+
--- test/scripts/securityTestData.sh
+++ test/scripts/securityTestData.sh
@@ -1,6 +1,15 @@
 #!/bin/sh
+set -e
 echo =====================================
 echo    Data for security test
+
+# Check if database exists first
+if mysql --host=opencatsdb --user=dev --password=dev -e "USE cats_test"; then
+    echo "Database cats_test exists."
+else
+    echo "Database cats_test does not exist. Creating it now."
+    mysql --host=opencatsdb --user=dev --password=dev -e "CREATE DATABASE cats_test"
+fi
 
 mysql --host=opencatsdb --user=dev --password=dev cats_test < data/test.sql
 mysql --host=opencatsdb --user=dev --password=dev cats_test < data/securityTests.sql
