6aae28a0e8ff55d2f252ca469ac18b5fb5a10d10
russellh@ysmail.net <russellh@ysmail.net>
2024-10-02 14:47:33 +0100

    testing

--- test/runAllTests.sh
+++ test/runAllTests.sh
@@ -16,28 +16,14 @@ fi
 
 php modules/tests/waitForDb.php
 
-# Check config.php for potential issues
-# cat config.php
-
-# Run PHPUnit tests
-./vendor/bin/phpunit src/OpenCATS/Tests/IntegrationTests
-./vendor/bin/phpunit src/OpenCATS/Tests/UnitTests
-
-
-
-# Ensure the database is available before running Behat
-RETRIES=5
-until mysql --host=opencatsdb --user=dev --password=dev -e "SHOW DATABASES" | grep -q "cats_test" || [ $RETRIES -eq 0 ]; do
-    echo "Re-checking for opencatsdb..."
-    RETRIES=$((RETRIES-1))
-    sleep 5
-done
-
-if [ $RETRIES -eq 0 ]; then
-    echo "ERROR: Unable to reconnect to opencatsdb before running Behat tests."
-    exit 1
-fi
 
 # Run Behat tests
 ./vendor/bin/behat -vv -c ./test/behat.yml --suite="default"
 ./vendor/bin/behat -vv -c ./test/behat.yml --suite="security"
+
+# Check config.php for potential issues
+# cat config.php
+
+# Run PHPUnit tests
+#./vendor/bin/phpunit src/OpenCATS/Tests/IntegrationTests
+
