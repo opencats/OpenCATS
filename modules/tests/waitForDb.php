bdb80dc24fdc1ea56e66c0f1f95216c02bfa7a92
russellh@ysmail.net <russellh@ysmail.net>
2024-10-02 14:36:16 +0100

    update testing

--- modules/tests/waitForDb.php
+++ modules/tests/waitForDb.php
@@ -3,23 +3,28 @@
 include_once('./config.php');
 include_once(LEGACY_ROOT . '/constants.php');
 include_once(LEGACY_ROOT . '/lib/DatabaseConnection.php');
+
 $canConnectAndSelectDb = false;
 $count = 30;
+
 while (! $canConnectAndSelectDb && $count > 0) {
-    $connection = @mysqli_connect(
-        DATABASE_HOST,
-        DATABASE_USER,
-        DATABASE_PASS
-    );
-    if ($connection) {
-        $isDBSelected = @mysqli_select_db($connection, DATABASE_NAME);
+    $connection = mysqli_connect(DATABASE_HOST, DATABASE_USER, DATABASE_PASS);
+
+    if (!$connection) {
+        echo "Connection attempt failed: " . mysqli_connect_error() . "\n";
+    } else {
+        $isDBSelected = mysqli_select_db($connection, DATABASE_NAME);
         if ($isDBSelected) {
             $canConnectAndSelectDb = true;
+        } else {
+            echo "Database selection failed: " . mysqli_error($connection) . "\n";
         }
     }
-    sleep(1);
+
+    sleep(5);
     --$count;
 }
+
 if ($canConnectAndSelectDb) {
     exit(0);
 } else {
