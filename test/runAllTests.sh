7ea691435654d56028f950c2da18f342ff7e52dc
russellh@ysmail.net <russellh@ysmail.net>
2024-10-02 14:02:28 +0100

    added more error reporting to runAllTests.sh

--- test/runAllTests.sh
+++ test/runAllTests.sh
@@ -2,23 +2,6 @@
 cd /var/www/public/
 dockerize -wait tcp://opencats_test_mariadb:3306 -wait http://opencats_test_web:80 -timeout 30s
 
-# Check if MySQL is accessible
-if ! mysql --host=opencatsdb --user=dev --password=dev -e "SHOW DATABASES"; then
-    echo "ERROR: Unable to connect to opencatsdb."
-    exit 1
-fi
-
-php modules/tests/waitForDb.php
-
-# Check config.php for potential issues
-cat config.php
-
-./vendor/bin/phpunit src/OpenCATS/Tests/IntegrationTests
-./vendor/bin/behat -vv -c ./test/behat.yml --suite="default"
-./vendor/bin/behat -vv -c ./test/behat.yml --suite="security"
-
-
-
 RETRIES=5
 until mysql --host=opencatsdb --user=dev --password=dev -e "SHOW DATABASES" || [ $RETRIES -eq 0 ]; do
     echo "Waiting for opencatsdb to be ready..."
@@ -30,3 +13,24 @@ if [ $RETRIES -eq 0 ]; then
     echo "ERROR: Unable to connect to opencatsdb."
     exit 1
 fi
+
+php modules/tests/waitForDb.php
+
+# Check config.php for potential issues
+cat config.php
+
+# Run PHPUnit tests
+./vendor/bin/phpunit src/OpenCATS/Tests/IntegrationTests
+
+# Check if the cats_test table is available before running Behat
+if ! mysql --host=opencatsdb --user=dev --password=dev -e "USE cats_test; SHOW TABLES';"; then
+    echo "ERROR: Database cats_test or the required table is not available."
+    exit 1
+fi
+
+# Run Behat tests
+./vendor/bin/behat -vv -c ./test/behat.yml --suite="default"
+./vendor/bin/behat -vv -c ./test/behat.yml --suite="security"
+
+
+
