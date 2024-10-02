129d63f7cbf2665b7700f64674982757c83d2ca7
russellh@ysmail.net <russellh@ysmail.net>
2024-10-02 13:45:20 +0100

    increased verbosity for behat errors

--- test/runAllTests.sh
+++ test/runAllTests.sh
@@ -14,8 +14,19 @@ php modules/tests/waitForDb.php
 cat config.php
 
 ./vendor/bin/phpunit src/OpenCATS/Tests/IntegrationTests
-./vendor/bin/behat -v -c ./test/behat.yml --suite="default"
-./vendor/bin/behat -v -c ./test/behat.yml --suite="security"
+./vendor/bin/behat -vv -c ./test/behat.yml --suite="default"
+./vendor/bin/behat -vv -c ./test/behat.yml --suite="security"
 
 
 
+RETRIES=5
+until mysql --host=opencatsdb --user=dev --password=dev -e "SHOW DATABASES" || [ $RETRIES -eq 0 ]; do
+    echo "Waiting for opencatsdb to be ready..."
+    RETRIES=$((RETRIES-1))
+    sleep 5
+done
+
+if [ $RETRIES -eq 0 ]; then
+    echo "ERROR: Unable to connect to opencatsdb."
+    exit 1
+fi
