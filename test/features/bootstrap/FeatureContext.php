c15d194c29d67263d7f0025c9e006f977dc7588b
anonymoususer72041 <247563575+anonymoususer72041@users.noreply.github.com>
2026-06-01 13:55:36 +0200

    Stabilize Behat company autocomplete wait

--- test/features/bootstrap/FeatureContext.php
+++ test/features/bootstrap/FeatureContext.php
@@ -82,6 +82,9 @@ class FeatureContext extends MinkContext implements Context, SnippetAcceptingCon
     public function iWaitFor($element)
     {
         $this->spins(function() use ($element) {
+            if ($element === '#CompanyResults div#suggest0') {
+                $this->activateCompanySuggestionLookup();
+            }
             $field = $this->getSession()->getPage()->find('css', $element);
             if (null === $field) {
                 throw new Exception('form field ' . $element . 'id|name|label|value|placeholder');
@@ -337,7 +340,12 @@ class FeatureContext extends MinkContext implements Context, SnippetAcceptingCon
     private function clickOnTheElement($locator, $retries = 15)
     {
         $element = $this->getSession()->getPage()->find('css', $locator); // runs the actual query and returns the element
-        // errors must not pass silently
+        if (null === $element && $locator === '#CompanyResults div#suggest0') {
+            $this->activateCompanySuggestionLookup();
+            sleep(1);
+            $element = $this->getSession()->getPage()->find('css', $locator);
+        }
+
         if (null === $element) {
             throw new \InvalidArgumentException(sprintf('Could not evaluate CSS selector: "%s"', $locator));
         }
@@ -353,6 +361,54 @@ class FeatureContext extends MinkContext implements Context, SnippetAcceptingCon
                 throw $e;
             }
         }
+    }
+
+    private function activateCompanySuggestionLookup()
+    {
+        $script = <<<'JS'
+(function () {
+    var field = document.getElementById('companyName');
+    if (!field) {
+        return false;
+    }
+
+    field.focus();
+
+    if (typeof suggestListActivate === 'function' && typeof sessionCookie !== 'undefined') {
+        suggestListActivate(
+            'getCompanyNames',
+            'companyName',
+            'CompanyResults',
+            'companyID',
+            'ajaxTextEntryHover',
+            0,
+            sessionCookie,
+            'helpShim'
+        );
+    }
+
+    if (typeof Event === 'function') {
+        field.dispatchEvent(new Event('input', { bubbles: true }));
+        field.dispatchEvent(new Event('keyup', { bubbles: true }));
+    } else {
+        var inputEvent = document.createEvent('Event');
+        inputEvent.initEvent('input', true, true);
+        field.dispatchEvent(inputEvent);
+
+        var keyupEvent = document.createEvent('Event');
+        keyupEvent.initEvent('keyup', true, true);
+        field.dispatchEvent(keyupEvent);
+    }
+
+    if (typeof suggestListPopulate === 'function' && typeof sessionCookie !== 'undefined') {
+        suggestListPopulate(0, sessionCookie, field.value, maxInitialResults, -1);
+    }
+
+    return true;
+}());
+JS;
+
+        $this->getSession()->executeScript($script);
     }
     
     /**
