28479a74c8b3dd16b8cd1d1f318cbbef31ca08ef
anonymoususer72041 <247563575+anonymoususer72041@users.noreply.github.com>
2026-06-08 11:45:45 +0200

    Finalize install schema version in test setup

--- test/runAllTests.sh
+++ test/runAllTests.sh
@@ -2,6 +2,7 @@
 cd /var/www/public/
 dockerize -wait tcp://opencats_test_mariadb:3306 -wait http://opencats_test_web:80 -timeout 30s
 php test/scripts/waitForDb.php
+php test/scripts/finalizeInstallSchemaVersion.php
 cat config.php
 ./vendor/bin/phpunit --testsuite IntegrationTests
 ./vendor/bin/behat -v -c ./test/behat.yml --suite="default"
